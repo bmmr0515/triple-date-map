97810a629f77165a84e55f074ef1a5670731f979
bmmr0515 <bmmr0515@example.com>
2026-05-20 11:07:14 +0900

    chore: ビルド時の不要なサイズ警告を非表示にするためchunkSizeWarningLimitを設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 巨大な外部ライブラリがあるため、ビルド時のサイズ警告基準値を 2000kB まで引き上げて警告を完全に非表示にします
+    chunkSizeWarningLimit: 2000,
+  }
 })
